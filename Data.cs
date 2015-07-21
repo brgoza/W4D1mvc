cade94d8203ea42494d62cf6c09ebf00cd5c0b10
Brandon Goza <brgoza@gmail.com>
2015-07-20 22:31:05 -0500

    Got Create, Details working... not going to implement Edit and Delete as I don't want to mess with it using a csv

--- Data.cs
+++ Data.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Web.WebPages;
 using W4D1mvc.Models;
 
 namespace W4D1mvc
 {
     public static class Data
     {
-        private static string defaultFilename = "~\\App_Data\\Classmates.csv";
-        public static List<Person> GetClassMates(string filename = "")
+        private static string defaultFilename = "\\App_Data\\Classmates.csv";
+
+        public static List<classmate> GetClassMatesFromCsv(string filename = "")
         {
+
             if (filename == "") filename = defaultFilename;
 
-            List<Person> peopleList = new List<Person>();
+            if (!System.IO.File.Exists(filename)) filename = GetNewFileName();
+
+            List<classmate> peopleList = new List<classmate>();
 
             var reader = System.IO.File.OpenText(filename);
+            int counter = 0;
             while (!reader.EndOfStream)
             {
                 var fields = reader.ReadLine().Split(',');
-                peopleList.Add(new Person(fields[0], fields[1], int.Parse(fields[2])));
+                peopleList.Add(new classmate(fields[0], fields[1], int.Parse(fields[2])));
+                counter++;
             }
             reader.Close();
             return peopleList;
         }
 
-        public static void AddClassMate(Person p, string filename)
+        private static string GetNewFileName()
         {
-            if (filename == "") filename = defaultFilename;
+            return HostingEnvironment.MapPath(@"~\App_Data\classmates.csv");
+        }
+
+        public static void AddClassMate(classmate p, string filename="")
+        {
+            if (filename == "") filename = GetNewFileName();
             var writer = System.IO.File.AppendText(filename);
-            writer.WriteLine(p.Name+","+p.HairColor+","+p.Height);
+            writer.WriteLine("\r\n" + p.Name + "," + p.HairColor + "," + p.Height);
             writer.Close();
-       
+
         }
     }
 }
