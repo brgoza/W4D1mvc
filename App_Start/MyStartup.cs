316e176ddfdff93b7388a5e4066567ca5c0bd7ec
Brandon Goza <brgoza@gmail.com>
2015-07-20 22:35:34 -0500

    Final initial commit

---
+++ App_Start/MyStartup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using W4D1mvc.Models;
+
+namespace W4D1mvc.App_Start
+{
+    public class MyStartup
+    {
+        public static List<classmate> Classmates = Data.GetClassMatesFromCsv();
+    }
+}
---
+++ Models/classmate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace W4D1mvc.Models
+{
+    public class classmate
+    {
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public int Height { get; set; }
+        public int Id { get; set; }
+        public static int classMateCount = 0;
+        public classmate(string name, string hairColor, int height)
+        {
+            Name = name;
+            HairColor = hairColor;
+            Height = height;
+            Id = classMateCount;
+            classMateCount++;
+        }
+    }
+}
